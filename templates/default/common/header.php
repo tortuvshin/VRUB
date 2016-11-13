0ff07e7b7f1108243b446952349616feda45a711
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-11-13 22:36:56 +0800

    Зочид буудал, амралтын газар #51

--- templates/default/common/header.php
+++ templates/default/common/header.php
@@ -168,47 +168,27 @@ if(ENABLE_COOKIES_NOTICE == 1 && !isset($_COOKIE['cookies_enabled'])){ ?>
         <div class="container-fluid">
             <div id="mainMenu" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
-                    <?php
-                    function subMenu($subpages)
-                    {
-                        global $parents;
-                        global $pages; ?>
-                        <ul class="subMenu">
-                            <?php
-                            foreach($subpages as $id_subpage){
-                                $subpage = $pages[$id_subpage]; ?>
-                                <li>
-                                    <?php
-                                    $nb_subpages = (isset($parents[$id_subpage])) ? count($parents[$id_subpage]) : 0; ?>
-                                    <a class="<?php if($nb_subpages > 0) echo " hasSubMenu"; ?>" href="<?php echo DOCBASE.$subpage['alias']; ?>" title="<?php echo $subpage['title']; ?>"><?php echo $subpage['name']; ?></a>
-                                    <?php if($nb_subpages > 0) subMenu($parents[$id_subpage]); ?>
-                                </li>
-                                <?php
-                            } ?>
-                        </ul>
-                        <?php
-                    }
-                    $nb_pages = count($pages);
-                    foreach($pages as $page_id_nav => $page_nav){
-                        if($page_nav['checked'] == 1){
-                            $id_parent = $page_nav['id_parent'];
-                            if($page_nav['main'] == 1 && ($id_parent == 0 || $id_parent == $homepage['id'])){ ?>
-                            
-                                <li class="primary nav-<?php echo $page_nav['id']; ?>">
-                                    <?php
-                                    if($page_nav['home'] == 1){ ?>
-                                        <a class="firstLevel<?php if($ishome) echo " active"; ?>" href="<?php echo DOCBASE.LANG_ALIAS; ?>" title="<?php echo $page_nav['title']; ?>"><?php echo $page_nav['name']; ?></a>
-                                        <?php
-                                    }else{
-                                        $nb_subpages = (isset($parents[$page_id_nav])) ? count($parents[$page_id_nav]) : 0; ?>
-                                        <a class="dropdown-toggle disabled firstLevel<?php if($nb_subpages > 0 && $page_nav['system'] != 1) echo " hasSubMenu"; if($page_nav['id'] == $page_id) echo " active"; ?>" href="<?php echo DOCBASE.$page_nav['alias']; ?>" title="<?php echo $page_nav['title']; ?>"><?php echo $page_nav['name']; ?></a>
-                                        <?php if($nb_subpages > 0 && $page_nav['system'] != 1) subMenu($parents[$page_id_nav]);
-                                    } ?>
-                                </li>
-                                <?php
-                            }
-                        }
-                    } ?>
+                    <li class="primary nav-1">
+                        <a class="dropdown-toggle disabled firstLevel" href="/" title="Нүүр">Нүүр</a>
+                    </li>
+
+                    <li class="primary nav-2">
+                        <a class="dropdown-toggle disabled firstLevel" href="/about-us" title="Бидний тухай">Бидний тухай</a>
+                    </li>
+                    <li class="primary nav-3">
+                        <a class="dropdown-toggle disabled firstLevel" href="/hotels/taracode-hotel" title="Зочид буудал">Зочид буудал</a>
+                    </li>
+                    <li class="primary nav-4">
+                        <a class="dropdown-toggle disabled firstLevel" href="/hotels/tourist-camp" title="Амралтын газар">Амралтын газар</a>
+                    </li>
+                    <li class="primary nav-5">
+                        <a class="dropdown-toggle disabled firstLevel" href="/sale" title="Мод захиалах">Мод захиалах</a>
+                    </li>
+                    <li class="primary nav-6">
+                        <a class="dropdown-toggle disabled firstLevel" href="/contact" title="Холбоо барих">Холбоо барих</a>
+                    </li>
+
+
                     <li class="primary">
                         <?php
                         if(isset($_SESSION['user'])){ ?>
